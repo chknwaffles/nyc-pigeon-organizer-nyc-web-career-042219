098012d6cb43055d79fa98cc9d20729656c5e286
Won Kim <wonsae.kim@gmail.com>
2019-04-23 15:34:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
+  list = {}
+  data.each do |catagory, hash|
+    hash.each do |k, arr|
+      arr.each do |e|
+        binding.pry if catagory == :gender
+        if list[e] == nil
+          list[e] = {catagory => []}
+        elsif list[e][catagory] == nil
+          list[e] = 
+        list[e][catagory] << k.to_s
+        
+      end
+    end
+  end
 end
