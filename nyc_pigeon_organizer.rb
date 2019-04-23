2cacffd92ab71ba4c757c59118a4af984ef8eba6
Won Kim <wonsae.kim@gmail.com>
2019-04-23 15:38:05 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
+  list = {}
+  data.each do |catagory, hash|
+    hash.each do |k, arr|
+      arr.each do |e|
+        if list[e] == nil
+          list[e] = {catagory => []}
+        end
+        
+        if list[e][catagory] == nil
+          list[e][catagory] = [k.to_s]
+        else
+          list[e][catagory] << k.to_s
+        end
+      end
+    end
+  end
+  list
 end
